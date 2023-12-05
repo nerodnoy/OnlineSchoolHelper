ad2e4d252dec5a621fd622967a0db72e152cc1a3
Artem Iliushin <thatsforsure@mail.ru>
2023-12-05 15:51:38 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+def generate_telegram_link(phone_number):
+    cleaned_phone_number = ''.join(char for char in phone_number if char.isnumeric() or char == '+')
+
+    if not cleaned_phone_number.startswith('+'):
+        cleaned_phone_number = '+' + cleaned_phone_number
+
+    telegram_link = f"t.me/{cleaned_phone_number}"
+    return telegram_link
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    telegram_link = None
+    if request.method == 'POST':
+        phone_number = request.form['phone_number']
+        telegram_link = generate_telegram_link(phone_number)
+    return render_template('index.html', telegram_link=telegram_link)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
