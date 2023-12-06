1d0eaeea4c9fd6dd02de1764762020f417442423
Artem Iliushin <thatsforsure@mail.ru>
2023-12-06 13:46:24 +0300

    expanding app

--- converter/app.py
+++ converter/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from converter.utils import generate_telegram_link
+from converter.questions import questions
 from dotenv import load_dotenv
 import os
 
@@ -17,6 +18,21 @@ def index():
         phone_number = request.form['phone_number']
         telegram_link = generate_telegram_link(phone_number)
     return render_template('index.html', telegram_link=telegram_link)
+
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    if request.method == 'POST':
+        answers = {}
+        for question in questions:
+            selected_option = request.form.get(question['question'])
+            answers[question['question']] = question['answers'].get(selected_option, '')
+
+        # Сохраняем ответы в сессии
+        session['answers'] = answers
+
+    # Передаем ответы в feedback.html для отображения
+    return render_template('feedback.html', questions=questions, answers=session.get('answers', {}))
 
 
 if __name__ == '__main__':
