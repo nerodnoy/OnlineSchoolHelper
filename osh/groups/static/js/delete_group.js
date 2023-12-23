10b4511d7bb7813269f26bcfacc5cb4442c8c88e
Artem Iliushin <thatsforsure@mail.ru>
2023-12-23 21:46:12 +0300

    layout tryout

--- osh/groups/static/js/delete_group.js
+++ osh/groups/static/js/delete_group.js
@@ -1,15 +1,26 @@
-function showConfirmationModal() {
-    document.getElementById('confirmationModal').classList.remove('hidden');
-}
+document.addEventListener('DOMContentLoaded', function () {
+  var deleteButton = document.getElementById('deleteButton');
+  var modal = document.getElementById('myModal');
+  var closeModal = document.getElementById('closeModal');
+  var confirmDeleteButton = document.getElementById('confirmDelete');
 
-function hideConfirmationModal() {
-    document.getElementById('confirmationModal').classList.add('hidden');
-}
+  deleteButton.addEventListener('click', function () {
+    modal.style.display = 'block';
+  });
 
-function showDeleteConfirmation(studentName, studentId) {
-    document.getElementById('confirmationModal_' + studentId).classList.remove('hidden');
-}
+  closeModal.addEventListener('click', function () {
+    modal.style.display = 'none';
+  });
 
-function hideDeleteConfirmation(studentId) {
-    document.getElementById('confirmationModal_' + studentId).classList.add('hidden');
-}
+  confirmDeleteButton.addEventListener('click', function () {
+    // Ваш код для удаления всех групп
+    alert('Все группы удалены!');
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
