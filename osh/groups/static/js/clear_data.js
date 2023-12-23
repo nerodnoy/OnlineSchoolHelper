07d8eac7e78d77ed5b3019b588e9f31b605b86f5
Iliushin Artyom <134132346+nerodnoy@users.noreply.github.com>
2023-12-23 22:07:59 +0300

    Delete osh/groups/static/js directory

--- osh/groups/static/js/clear_data.js
+++
@@ -1,7 +0,0 @@
-function showClearConfirmationModal() {
-    document.getElementById('clearConfirmationModal').classList.remove('hidden');
-}
-
-function hideClearConfirmationModal() {
-    document.getElementById('clearConfirmationModal').classList.add('hidden');
-}
