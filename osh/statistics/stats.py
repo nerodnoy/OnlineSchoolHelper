84226137f70ac72b13ab8b6e4bc68686308e619a
Artem Iliushin <thatsforsure@mail.ru>
2024-01-13 13:19:24 +0300

    fixed stats abstractions

--- osh/statistics/stats.py
+++ osh/statistics/stats.py
@@ -1,14 +1,9 @@
 from flask import (
     Blueprint,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    # abort
+    render_template
 )
 
-from osh.database.database import get_groups_for_month, db_get_all_groups
-from osh.statistics.stats_utility import get_current_month, translate_month_name
+from osh.statistics.stats_utility import prepare_stats_data
 
 stats_bp = Blueprint('stats', __name__,
                      static_folder='static',
@@ -18,36 +13,6 @@ stats_bp = Blueprint('stats', __name__,
 
 @stats_bp.route('/', methods=['GET'])
 def stats():
-    # все группы
-    groups = db_get_all_groups()
+    stats_data = prepare_stats_data()
 
-    # текущий месяц
-    current_month = get_current_month()
-
-    # выбранный месяц из параметра запроса
-    selected_month = request.args.get('selected_month')
-    selected_month = selected_month or current_month
-
-    translated_month = translate_month_name(selected_month)
-
-    # фильтруем группы для выбранного месяца
-    groups_for_month = sorted(
-        [group for group in groups if group['month'] == selected_month],
-        key=lambda x: x['start_date']
-    )
-
-    # Количество групп в месяце
-    total_groups = len(groups_for_month)
-
-    # Итоговая оплата за месяц
-    total_payment = sum(
-        3250 if 'PRO' in group['name'] or 'START' in group['name']
-        else 6000 for group in groups_for_month)
-
-    return render_template('stats.html',
-                           groups_for_month=groups_for_month,
-                           current_month=translated_month,
-                           groups=groups,
-                           total_groups=total_groups,
-                           total_payment=total_payment
-                           )
+    return render_template('stats.html', **stats_data)
--- osh/statistics/stats_utility.py
+++ osh/statistics/stats_utility.py
@@ -1,24 +1,36 @@
-from datetime import datetime
+from flask import request
+
+from osh.database.database import db_get_all_groups
+from osh.groups.utility.handlers.calendar import get_current_month, translate_month_name
 
 
-def get_current_month():
-    return datetime.now().strftime('%B')
+def prepare_stats_data():
+    groups = db_get_all_groups()
 
+    current_month = get_current_month()
 
-def translate_month_name(month_name):
-    month_translation = {
-        'January': 'Январь',
-        'February': 'Февраль',
-        'March': 'Март',
-        'April': 'Апрель',
-        'May': 'Май',
-        'June': 'Июнь',
-        'July': 'Июль',
-        'August': 'Август',
-        'September': 'Сентябрь',
-        'October': 'Октябрь',
-        'November': 'Ноябрь',
-        'December': 'Декабрь'
+    selected_month = request.args.get('selected_month')
+    selected_month = selected_month or current_month
+
+    translated_month = translate_month_name(selected_month)
+
+    groups_for_month = sorted(
+        [group for group in groups if group['month'] == selected_month],
+        key=lambda x: x['start_date']
+    )
+
+    # Количество групп в месяце
+    total_groups = len(groups_for_month)
+
+    # Итоговая оплата за месяц
+    total_payment = sum(
+        3250 if 'PRO' in group['name'] or 'START' in group['name']
+        else 6000 for group in groups_for_month)
+
+    return {
+        'groups_for_month': groups_for_month,
+        'current_month': translated_month,
+        'groups': groups,
+        'total_groups': total_groups,
+        'total_payment': total_payment
     }
-
-    return month_translation.get(month_name, month_name)
